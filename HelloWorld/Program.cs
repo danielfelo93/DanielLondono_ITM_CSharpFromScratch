5026891be3441cfc558b677fab97d6eb8335eefb
Daniel Londoño <danielfelo93@gmail.com>
2024-09-16 13:00:03 -0500

    Changed HelloWorld by class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HelloWorld{
+    
+    class Program{
+        static void Main(){
+
+            const string Name="Daniel";
+            Console.WriteLine($"HelloWorld! {Name}");
+            Console.WriteLine();
+        }
+    }
+}
