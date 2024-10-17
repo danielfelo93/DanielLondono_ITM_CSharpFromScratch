2d3a200f2b4245bcda0aae00bc39696dbc84fe8c
Daniel Londoño <danielfelo93@gmail.com>
2024-10-16 20:52:51 -0500

    final

---
+++ final/Challenge1.cs
@@ -0,0 +1,247 @@
+namespace itm.csharp.basic {
+    public class Challenge1 {
+        public void Run()
+        {
+            Console.WriteLine("Ingrese un número: ");
+            int num = int.Parse(Console.ReadLine());
+            if(num > 0) Console.WriteLine("Positivo");
+            else if (num < 0) Console.WriteLine("Negativo");
+            else Console.WriteLine("Es cero");
+        }
+    }
+
+    public class Challenge2 {
+        public void Run()
+        {
+            Console.WriteLine("Ingrese el primer número: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Suma: {num1+num2}\nResta: {num1-num2}\nMultiplicación: {num1*num2}\nDivisión: {num1/num2}");
+        }
+    }
+
+    public class Challenge3 {
+        public void Run()
+        {
+            Console.WriteLine("Ingrese un número: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine($"El cuadrado de {numero} es: {numero*numero}");
+        }
+    }
+
+    public class Challenge4 {
+        public void Run()
+        {
+            Console.WriteLine("Ingrese el radio del círculo: ");
+            double radio = double.Parse(Console.ReadLine());
+            double area = Math.PI * radio * radio;
+            Console.WriteLine($"El área del círculo de radio {radio} es: {area}");
+        }
+    }
+
+    public class Challenge5 {
+        public void Run()
+        {
+            Console.WriteLine("Ingrese un número entre 1 y 7: ");
+            int dia = int.Parse(Console.ReadLine());
+            switch(dia)
+            {
+                case 1: Console.WriteLine("Lunes"); break;
+                case 2: Console.WriteLine("Martes"); break;
+                case 3: Console.WriteLine("Miercoles"); break;
+                case 4: Console.WriteLine("Jueves"); break;
+                case 5: Console.WriteLine("Viernes"); break;
+                case 6: Console.WriteLine("Sábado"); break;
+                case 7: Console.WriteLine("Domingo"); break;
+                default: Console.WriteLine("Número inválido"); break;
+            }
+            
+        }
+    }
+
+     public class Challenge6 {
+        public void Run()
+        {
+            Console.WriteLine("Ingrese su salario mensual: ");
+            double salario = double.Parse(Console.ReadLine());
+            double impuesto = salario > 1000 ? 0.1 * salario : 0;
+            Console.WriteLine($"El impuesto a pagar es: {impuesto}");
+        }
+    }
+
+    public class Challenge7 {
+        public void Run()
+        {
+            try {
+            Console.WriteLine("Número a dividir: ");
+            double n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Divisor: ");
+            double divisor = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Resultado: {n/divisor}");
+            }
+            catch (DivideByZeroException)
+            {
+            Console.WriteLine("No se puede dividir por cero");
+            }
+            catch (Exception)
+            {
+            Console.WriteLine("Error al realizar la operación");
+            }
+
+        }
+    }
+
+    public class Challenge8 {
+        public void Run()
+        {
+            int suma = 0;
+            for (int i = 1; i<= 100; i+= 2){
+                suma+=i;
+            }
+            Console.WriteLine($"La suma de números impares entre 1 y 100 es: {suma}");
+        }
+    }
+
+    public class Challenge9 {
+        public void Run()
+        {
+            Console.WriteLine("Ingrese el primer numerador: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el primer denominador: ");
+            int den1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segungo numerador: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo denominador: ");
+            int den2 = int.Parse(Console.ReadLine());
+
+            try {
+                Fraccion fraccion1 = new Fraccion(num1, den1);
+                Fraccion fraccion2 = new Fraccion(num2, den2);
+
+                Fraccion resultado = fraccion1.Sumar(fraccion2);
+
+                Console.WriteLine($"La suma de {fraccion1} y {fraccion2} es: {resultado}");
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        
+        }
+    }
+
+    public class Fraccion {
+        public int Numerador {get; private set;}
+        public int Denominador {get; private set;}
+
+        public Fraccion(int numerador, int denominador) {
+            if(denominador == 0) {
+                throw new ArgumentException("El denominador no puede ser cero.");
+            }
+            Numerador = numerador;
+            Denominador = denominador;
+        }
+
+        public Fraccion Sumar(Fraccion otra) {
+            int nuevoNumerador = Numerador * otra.Denominador + otra.Numerador * Denominador;
+            int nuevoDenominador = Denominador * otra.Denominador;
+            return new Fraccion(nuevoNumerador, nuevoDenominador);
+        }
+
+        public override string ToString(){
+            return $"{Numerador}/{Denominador}";
+        }
+    }
+
+    public class Challenge10 {
+        public void Run()
+        {
+            Console.WriteLine("Ingrese una palabra: ");
+            string palabra = Console.ReadLine();
+            string invertida = new string(palabra.Reverse().ToArray());
+            Console.WriteLine($"La palabra invertida es: {invertida}");
+        }
+    }
+
+    public class Challenge11 {
+        public void Run()
+        {
+            Console.WriteLine("Ingrese el primer número: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer número: ");
+            double num3 = double.Parse(Console.ReadLine());
+
+            double promedio = (num1+num2+num3)/3;
+            Console.WriteLine($"El promedio de los tres números es: {promedio}");
+        }
+    }
+
+    public class Challenge12 {
+        public void Run()
+        {
+            List<int> numeros = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ingrese el número: {i + 1}: ");
+                 int numero = int.Parse(Console.ReadLine());
+                numeros.Add(numero);
+            }
+
+            Console.WriteLine($"El número más grande de la lista es: {numeros.Max()}");
+        }
+    }
+
+    public class Challenge13 {
+        public void Run()
+        {
+            Console.WriteLine("Introduce una palabra:");
+            string palabra = Console.ReadLine();
+
+            if (palabra== new string(palabra.Reverse().ToArray()))
+            {
+                Console.WriteLine($"La palabra {palabra} es un palindromo.");
+            }
+            else
+            {
+                Console.WriteLine($"La palabra {palabra} no es un palindromo.");
+            } 
+        }
+    }
+
+    public class Challenge14 {
+        public void Run()
+        {
+            Console.WriteLine("Introduce un número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero%2 == 0)
+            {
+                Console.WriteLine($"El número {numero} es par.");
+            }
+            else
+            {
+                Console.WriteLine($"El número {numero} es impar.");
+            } 
+        }
+    }
+
+    public class Challenge15 {
+        public void Run()
+        {
+            Console.WriteLine("Introduce un número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero>= 0 && numero <= 10)
+            {
+                Console.WriteLine($"El número {numero} está en el rango de 0 a 10.");
+            }
+            else
+            {
+                Console.WriteLine($"El número {numero} está fuera del rango de 0 a 10.");
+            } 
+        }
+    }
+}
--- final/Program.cs
+++ final/Program.cs
@@ -9,16 +9,81 @@ class Program {
 
             string entrada = Console.ReadLine().ToLower();
             if(entrada == "q" || entrada == "exit") {
-                breack; //Salir del bucle
+                break; //Salir del bucle
             }
 
             if(int.TryParse(entrada, out int eleccion) && eleccion >= 1 && eleccion <= 15) {
-                //logica del switch
+                switch(eleccion)
+                {
+                    case 1:
+                        var Challenge1 = new Challenge1();
+                        Challenge1.Run();
+                        break;
+                    case 2:
+                        var Challenge2 = new Challenge2();
+                        Challenge2.Run();
+                        break;
+                    case 3:
+                        var Challenge3 = new Challenge3();
+                        Challenge3.Run();
+                        break;
+                    case 4:
+                        var Challenge4 = new Challenge4();
+                        Challenge4.Run();
+                        break;
+                    case 5:
+                        var Challenge5 = new Challenge5();
+                        Challenge5.Run();
+                        break;
+                    case 6:
+                        var Challenge6 = new Challenge6();
+                        Challenge6.Run();
+                        break;
+                    case 7:
+                        var Challenge7 = new Challenge7();
+                        Challenge7.Run();
+                        break;
+                    case 8:
+                        var Challenge8 = new Challenge8();
+                        Challenge8.Run();
+                        break;
+                    case 9:
+                        var Challenge9 = new Challenge9();
+                        Challenge9.Run();
+                        break;
+                    case 10:
+                        var Challenge10 = new Challenge10();
+                        Challenge10.Run();
+                        break;
+                    case 11:
+                        var Challenge11 = new Challenge11();
+                        Challenge11.Run();
+                        break;
+                    case 12:
+                        var Challenge12 = new Challenge12();
+                        Challenge12.Run();
+                        break;
+                    case 13:
+                        var Challenge13 = new Challenge13();
+                        Challenge13.Run();
+                        break;
+                    case 14:
+                        var Challenge14 = new Challenge14();
+                        Challenge14.Run();
+                        break;
+                    case 15:
+                        var Challenge15 = new Challenge15();
+                        Challenge15.Run();
+                        break;
+                    default:
+                        Console.WriteLine("Peresiona una tecla para continuar...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        Menu.MostrarMenu();
+                        break;
+                }
 
-                Console.WriteLine("Peresiona una tecla para continuar...");
-                Console.ReadKey();
-                Console.Clear();
-                Menu.MostrarMenu();
+                
             }
             else {
                 Console.WriteLine("Opción no valida. Por favor, ingrese un numero entre 1 y 15, o 'q' o 'exit' para salir.");
